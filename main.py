3fb798f77895091167a3b75abc1ca3f5bb31738a
Garland Lai <garlandlai24@gmail.com>
2020-08-31 16:16:13 -0700

    add some methods that will be used in game

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from classes.game import Person, Bcolors
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Lighting", "cost": 10, "dmg": 60},
-         {"name": "Water", "cost": 10, "dmg": 60}]
+         {"name": "Lighting", "cost": 15, "dmg": 80},
+         {"name": "Water", "cost": 12, "dmg": 50}]
 
 player = Person(460, 65, 60, 34, magic)
 
-print(player.generate_damage())
-print(player.generate_damage())
-print(player.generate_damage())
+print("Attack Damage:", player.generate_damage())
+print("Spell Damage:", player.generate_spell_damage(1))
+print("Spell Damage:", player.generate_spell_damage(2))
