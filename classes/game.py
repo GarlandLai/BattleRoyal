134dea6b0b20cced971fec85b3a0201e78f41e81
Garland Lai <garlandlai24@gmail.com>
2020-09-05 12:22:15 -0700

    implement the mp hit points as well

--- classes/game.py
+++ classes/game.py
@@ -102,20 +102,30 @@ class Person:
         # Checking the length of hp
         hp_string = str(self.hp) + "/" + str(self.maxhp)
         current_hp = ""
-
         if len(hp_string) < 9:
             decreased = 9 - len(hp_string)
             while decreased > 0:
                 current_hp += " "
                 decreased -= 1
-
             current_hp += hp_string
         else:
             current_hp = hp_string
 
+        # Checking the length of hp
+        mp_string = str(self.mp) + "/" + str(self.maxmp)
+        current_mp = ""
+        if len(mp_string) < 7:
+            decreased = 7 - len(mp_string)
+            while decreased > 0:
+                current_mp += " "
+                decreased -= 1
+            current_mp += mp_string
+        else:
+            current_mp = mp_string
+
         print("                 _________________________            __________")
         print(Bcolors.BOLD + self.name + current_hp + " |" +
               Bcolors.OKGREEN + hp_bar +
-              Bcolors.ENDC + Bcolors.BOLD + "|  " + str(self.mp) + "/" + str(self.maxmp) + " |" +
+              Bcolors.ENDC + Bcolors.BOLD + "|  " + current_mp + " |" +
               Bcolors.OKBLUE + mp_bar +
               Bcolors.ENDC + Bcolors.BOLD + "|")
